c836dfdc0e0e5e6065c38ebc006c2aabd3f523d3
hulj07001 <hulj07001@E-PW05CGR7>
2024-10-09 11:58:34 +0200

    Add project files.

---
+++ kap2 upp2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace kap2upp2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hej! Vilken är din favorit bok? ");
+            string favbok=Console.ReadLine();
+            Console.WriteLine("Din favorit bok är alltså " + favbok + "?");
+        }
+    }
+}
